48c695ea2364f76b2916750ec8387ce27b6ec79e
Adam Nawrocki <adam.nawroc@gmail.com>
2015-01-12 13:43:26 -0500

    Grab entire script

--- sein.py
+++ sein.py
@@ -4,7 +4,7 @@ webpage = urllib.urlopen("http://www.seinology.com/scripts/script-08.shtml").rea
 
 start = webpage.find("=====================<br>")
 step2 = webpage.find(">", start)
-step3 = webpage.find("</head>", step2+1)
+step3 = webpage.lower().find("the end", step2+1)
 
 output = webpage[step2+1:step3]
 
