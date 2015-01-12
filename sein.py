3421326b9673bb3456a6bf795d120bfad49cec02
Adam Nawrocki <adam.nawroc@gmail.com>
2015-01-12 13:58:06 -0500

    Remove tabs from the beginning of newlines

--- sein.py
+++ sein.py
@@ -8,6 +8,8 @@ step3 = webpage.lower().find("the end", step2+1)
 
 output = webpage[step2+1:step3] + "The End"
 
+output = output.replace("\t", "")
+
 f = open("new_file.txt", 'w')
 f.write(output)
 f.close()
