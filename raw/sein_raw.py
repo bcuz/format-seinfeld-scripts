18a642bf964a1e07a5e5f01721b4d4f18d8825f4
Adam Nawrocki <adam.nawroc@gmail.com>
2015-01-22 10:07:08 -0500

    Save point

--- raw/sein_raw.py
+++ raw/sein_raw.py
@@ -1,6 +1,8 @@
 import urllib
 from sys import argv
 
+# This file saves the raw html of each episode in it's own file
+
 # after "py filename.py" you put the first script you want up to and including the last script
 # It'll download all scripts in between. If you just want one script, you enter the same number twice
 script, start_num, end_num = argv
@@ -43,8 +45,7 @@ def magic(start_num):
 		# deals with the rest of the episodes
 		else:
 			webpage = urllib.urlopen("http://www.seinology.com/scripts/script-" + str(num) + ".shtml").read()
-		
-		# uses the find_nth function to find the second instance of " - ", which is nearby the title of the script
+
 		find_title = find_nth(webpage, " - ") 
 		# finds the "-" character in the webpage, but it starts looking at the point where find_title is found. 
 		# So we're really only moving the cursor from the first character of " - " to the "-" character itself
@@ -55,45 +56,7 @@ def magic(start_num):
 
 		# the title starts to be grabbed at the character right after "- " and ends being extracted at the character
 		# right before "<"
-		title = webpage[begin_title+2:end_title]
-
-		#find the index where the scripts begin
-		find_script = webpage.find("=====<br>")
-		
-		# if "=====<br>" is not found, look for "=&nbsp;<br>", which is how two episodes are formatted instead
-		if find_script == -1:
-			find_script = webpage.find("=&nbsp;<br>")
-		if find_script == -1:
-			find_script = webpage.find("=</font></p>")	
-
-		#go to the last character of find_script	
-		begin_script = webpage.find(">", find_script)
-		#start looking for the end of script after the index of begin_script. Episodes end when there's a </td>
-		end_script = webpage.lower().find("</td>", begin_script+1)
-		# the script starts at the character after ">" and ends at the character just befoer </td> This extracts
-		# all the information between these two points
-		output = webpage[begin_script+1:end_script]
-
-		# tidying up the script contents
-		replace_with_quote = ["&quot;", "&#148;", "&#147;"]
-
-		for special_char in replace_with_quote:
-			output = output.replace(special_char, '"')
-
-		replace_with_empty_space = ["\t", "<br>", '<font size="-2">', '</font>', '<p>', '</p>']
-
-		for special_char in replace_with_empty_space:
-			output = output.replace(special_char, '')	
-			
-		output = output.replace("&nbsp;", "") # this might cause problems, but for now it gets rid of special code
-		output = output.replace("&rsquo;", "'")
-		output = output.replace("&#146;", "'")
-		output = output.replace("&#145;", "'")
-		output = output.replace("&#150;", "-")
-		output = output.replace("&#133;", "...") 
-		output = output.replace("&amp;", "&")
-		output = output.replace("\n\n\n", "\n\n")
-		output = output.replace("\n\n\n\n", "\n\n\n")
+		title = webpage[begin_title+2:end_title]			
 
 		season1 = range(1, 6)
 		season2 = range(6, 18)
@@ -125,9 +88,9 @@ def magic(start_num):
 			episode = "s9e" + str(season9.index(num)+1)
 
 		# open a file with the designated title in writing mode
-		f = open("Seinfeld " + episode + " " + str(num) + " - " + title + ".txt", 'w')
+		f = open("Seinfeld " + str(num) + ".txt", 'w')
 		# write the script to the file
-		f.write(output)
+		f.write(webpage)
 		f.close()
 
 
